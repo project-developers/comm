72051bb0f26d51ee1a3f03eca216d856b6f15ff3
project-developers <80718041+project-developers@users.noreply.github.com>
2021-04-15 17:57:18 +0000

    Update mainAns.js

--- mainAns.js
+++ mainAns.js
@@ -199,7 +199,7 @@ function onReceiveMessageCallback(event) {
 
   console.log(`Received Message ${event.data.byteLength}`);
   receiveBuffer.push(`${event.data}`);
-  receivedSize += Number(`${event.data.byteLength}`);
+  receivedSize += `${event.data.byteLength}`;
   receiveProgress.value = receivedSize;
 
   // we are assuming that our signaling protocol told
