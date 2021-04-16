f0f47685ded7bb6d83abf6c7439b3902da18fd0a
project-developers <80718041+project-developers@users.noreply.github.com>
2021-04-16 19:02:46 +0000

    Update main.js

--- main.js
+++ main.js
@@ -60,7 +60,7 @@ async function createConnection() {
     sendData();
   }else if(sendChannel && localConnection){
     sendChannel.close();
-    sendChannel = localConnection.createDataChannel('sendDataChannel', {maxPacketLifeTime: 120000});
+    sendChannel = localConnection.createDataChannel('sendDataChannel', {maxPacketLifeTime: 120000}, {maxRetransmits: 64});
     sendData();
   }else{
     clickcreateoffer();
