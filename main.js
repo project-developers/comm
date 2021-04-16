f8d3ca58b8298ecb5b7781499b1b39ecb8ceb594
project-developers <80718041+project-developers@users.noreply.github.com>
2021-04-16 21:49:55 +0000

    Update main.js

--- main.js
+++ main.js
@@ -162,6 +162,20 @@ function sendData() {
   //sendChannel.close();
 }
 
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+/*
+const list = [1, 2, 3, 4]
+const doSomething = async () => {
+  for (const item of list) {
+    await sleep(1000)
+    console.log('waiting...')    
+  }
+}
+
+doSomething()
+*/
 function closeDataChannels() {
   console.log('Closing data channels');
   if (sendChannel) {
