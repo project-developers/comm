cb6fdcc0b458edfb91074486e92273e23fceb2d5
project-developers <80718041+project-developers@users.noreply.github.com>
2021-04-18 00:10:10 +0000

    Update offer.js

--- offer.js
+++ offer.js
@@ -10,7 +10,7 @@ function clickcreateoffer() {
   chatChannel.onmessage = datachannelmessage;
   */
   //sendChannel = localConnection.createDataChannel('sendDataChannel');
-  sendChannel = localConnection.createDataChannel('sendDataChannel', {maxPacketLifeTime: 268435456}, {maxRetransmits: 1048576});
+  sendChannel = localConnection.createDataChannel('sendDataChannel', {maxPacketLifeTime: 32768}); //, {maxRetransmits: 1048576});
   //sendChannel = peerConnection.createDataChannel(fileInput.files[0].name + ' | ' + fileInput.files[0].size + ' | ' + fileInput.files[0].type + ' | ' + fileInput.files[0].lastModified);
   
   sendChannel.binaryType = 'arraybuffer';
